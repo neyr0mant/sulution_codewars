0dc8b2179306df083a391a7950c956e70eb72aa6
Neyromant <Neyromant777@gmail.com>
2024-03-03 14:19:25 +0300

    123

--- Who_is_Mr_Wrong.py
+++ Who_is_Mr_Wrong.py
@@ -1,8 +1,7 @@
 conversation1=[
 "John:I'm in 1st position.",
 "Peter:I'm in 2nd position.",
-"Tom:I'm in 1st position.",
-"Peter:The man behind me is Tom."
+"Tom1:I'm in 3st position.",
 ]
 
 conversation2=[
@@ -43,9 +42,27 @@ conversation5 = ['Mnulesil:The man in front of me is Reoowpqgn.',
  "Rynuk:I'm in 9th position.",
  'Aiaug:There are 7 people behind me.']
 list_conversation = [conversation1, conversation2,conversation3,conversation4, conversation5]
-list_conversation = [i for idx, i in enumerate(list_conversation) if idx in [1]]
+list_conversation = [i for idx, i in enumerate(list_conversation) if idx in [0]]
 import re
 import json
+
+def get_data_for_list_str(list_str):
+    dict_rule_txt_for_name = {}
+    for rule_str in list_str:
+        conversation_list = rule_str.split(":")
+        name, rule_txt = conversation_list
+        if dict_rule_txt_for_name.get(name):
+            dict_rule_txt_for_name[name].append(rule_txt)
+        else:
+            dict_rule_txt_for_name[name] = [rule_txt]
+    set_name = set(dict_rule_txt_for_name.keys())
+    len_set_name = len(set_name)
+    data_for_name = {}
+    for name, list_data in dict_rule_txt_for_name.items():
+        for data_str in list_data:
+            data_for_name.update({name: get_params_str(data_str, len_set_name)})
+    return data_for_name
+
 def get_params_str(str_in, queue_length):
     find_int = re.findall(r'\d+', str_in)
     value_params, key_params = '', ''
@@ -66,60 +83,45 @@ def get_params_str(str_in, queue_length):
     return {key_params: value_params}
 
 
-def assert_composition_rule(data_in):
+def use_rule_and_assert_composition(data_in, rule):
+    print()
+
+
+def get_data_position_and_rule(data_in):
     len_names = len(data_in.keys())
-    all_position = set([i for i in range(1, len_names+1)])
-    position_for_name = {}
+    name_with_position = {}
     name_not_position = {}
-    list_name_mr_wrong = []
+    potential_mr_wrong = []
     for name, data in data_in.items():
         position = data.get("position")
         if position:
-            exist_name = position_for_name.get(position)
+            exist_name = name_with_position.get(position)
             if exist_name:
-                list_name_mr_wrong.extend([name, exist_name])
+                name_add = exist_name + [name]
+                name_with_position[position] = name_add
+                potential_mr_wrong.extend(name_add)
             else:
-                position_for_name.update({position: name})
+                name_with_position[position] = [name]
         else:
             name_not_position.update({name: data})
-    position_found = set(list(position_for_name.keys()))
-    if len(position_found) == len_names:
-        return True, position_for_name
-    position_free = all_position - position_found
-    while position_found != all_position:
-        for not_position, data_no_position in name_not_position.items():
-            rule_no_position, name_ = list(data_no_position.items())[0]
-            print()
-    return (True, position_for_name) if len(position_for_name.keys()) == len_names else (False, list_name_mr_wrong)
+    position_found = list(name_with_position.keys())
+    #Если исходные данные не противоречивы изначально по позициям или в исходных данных по позициям больше 2 вруноы
+    # тогда лжецев или нет, или их четное количество - не подходит по условиям
+    assert_bool = [len(position_found) != len_names, len(potential_mr_wrong) <= 2]
+    return bool(all(assert_bool)), name_with_position, name_not_position, potential_mr_wrong
+
 
 def find_out_mr_wrong(conversation):
-    dict_rule_txt_for_name = {}
-    for conversation_ in conversation:
-        conversation_list = conversation_.split(":")
-        name, rule_txt = conversation_list
-        if dict_rule_txt_for_name.get(name):
-            dict_rule_txt_for_name[name].append(rule_txt)
-        else:
-            dict_rule_txt_for_name[name] = [rule_txt]
-    set_name = set(dict_rule_txt_for_name.keys())
-    len_set_name = len(set_name)
-    data_for_name = {}
-    for name, list_data in dict_rule_txt_for_name.items():
-        for data_str in list_data:
-            data_for_name.update({name: get_params_str(data_str, len_set_name)})
-    exist_composition, list_name_mr_wrong = assert_composition_rule(data_for_name)
-    if exist_composition:
+    data_for_name = get_data_for_list_str(conversation)
+    assert_composition, name_with_position, name_not_position, potential_mr_wrong = (
+        get_data_position_and_rule(data_for_name))
+    if not assert_composition:
         return None
-    data_mr_right = {name:data_name for name, data_name in data_for_name.items() if name not in list_name_mr_wrong}
-    data_mr_wrong = {name:data_name for name, data_name in data_for_name.items() if name in list_name_mr_wrong}
-    not_key_err = False
-    for mr_wrong in list_name_mr_wrong:
-        data_mr_right.update({mr_wrong:data_for_name[mr_wrong]})
-        res_assert, type_not_composition = assert_composition_rule(data_mr_right)
-        if not_key_err and res_assert:
-            print()
-        del data_mr_right[mr_wrong]
-        not_key_err = True if type_not_composition == "not_key" else not_key_err
+    print(json.dumps(data_for_name, indent=2))
+    print(json.dumps(name_with_position, indent=2))
+    print(json.dumps(name_not_position, indent=2))
+
+
 
 
 for idx, conversation in enumerate(list_conversation):
