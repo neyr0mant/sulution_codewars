1d12b9faa0664520ac28dc8864aea6f8a41da3a4
Neyromant <Neyromant777@gmail.com>
2024-03-04 12:28:17 +0300

    123

--- Who_is_Mr_Wrong.py
+++ Who_is_Mr_Wrong.py
@@ -7,7 +7,7 @@ conversation1=[
 
 conversation2=[
 "John:I'm in 1st position.",
-"Peter:I'm in 12nd position.",
+"Peter:I'm in 2nd position.",
 "Tom:I'm in 1st position.",
 "Peter:The man in front of me is Tom."
 ]
@@ -25,7 +25,8 @@ conversation4=[
 "Tom:There are 2 people behind me.",
 "Peter:The man behind me is Tom."
 ]
-conversation5 = ['Mnulesil:The man in front of me is Reoowpqgn.',
+conversation5 = [
+    'Mnulesil:The man in front of me is Reoowpqgn.',
  'Sene:There are 6 people in front of me.',
  "Qqeod:I'm in 6th position.",
  'Gdimeia:There are 9 people behind me.',
@@ -41,15 +42,21 @@ conversation5 = ['Mnulesil:The man in front of me is Reoowpqgn.',
  'Reoowpqgn:There are 3 people in front of me.',
  'Mnulesil:There are 5 people behind me.',
  "Rynuk:I'm in 9th position.",
- 'Aiaug:There are 7 people behind me.']
+ 'Aiaug:There are 7 people behind me.'
+]
 
 conversation6=[
 "John:I'm in 1st position.",
 "Peter:I'm in 2nd position.",
 "Tom:I'm in 1st position.",
 ]
-list_conversation = [conversation1, conversation2,conversation3,conversation4, conversation5, conversation6]
-# lsit_acssert = [1,2,3,4,5,6]
+conversation7 = ["Tom:The man behind me is Bob.",
+      "Bob:The man in front of me is Tom.",
+      "Bob:The man behind me is Gary.",
+      "Gary:The man in front of me is Bob.",
+      "Fred:I'm in 1st position."]
+list_conversation = [conversation1, conversation2,conversation3,conversation4, conversation5, conversation6, conversation7]
+# lsit_acssert = [0,1, 2,3,4,5,6]
 lsit_acssert = [0]
 list_conversation = [i for idx, i in enumerate(list_conversation) if idx in lsit_acssert]
 import re
@@ -64,13 +71,42 @@ def get_data_for_list_str(list_str):
             dict_rule_txt_for_name[name].append(rule_txt)
         else:
             dict_rule_txt_for_name[name] = [rule_txt]
-    set_name = set(dict_rule_txt_for_name.keys())
-    len_set_name = len(set_name)
+    all_name = set(dict_rule_txt_for_name.keys())
+    len_set_name = len(all_name)
+    all_position = {i for i in range(1, len_set_name+1)}
     data_for_name = {}
+    mr_wrong = False
+    list_name_same_position = []
+    data_position = {}
+    data_link_name = {}
+    position_find = set()
+    position_for_name = {}
     for name, list_data in dict_rule_txt_for_name.items():
         for data_str in list_data:
-            data_for_name.update({name: get_params_str(data_str, len_set_name)})
-    return data_for_name
+            cur_params = data_for_name.get(name, {})
+            key_params, value_params = get_params_str(data_str, len_set_name)
+            if key_params in ["position_plus", "position_minus"]:
+                if value_params == name: # когда человек ссылается сам на себя - значит он врун
+                    mr_wrong = name
+                    break
+            if isinstance(value_params, int):
+                data_position.update({name: {key_params: value_params}})
+                exist_position = position_for_name.get(value_params)
+                position_for_name.update({value_params:[name]})
+                if value_params in position_find:
+                    if exist_position:
+                        new_mr_wrong_list = exist_position + [name]
+                        if position_for_name.get(value_params):
+                            list_name_same_position = new_mr_wrong_list
+                position_find.add(value_params)
+            else:
+                data_link_name.update({name: {key_params: value_params}})
+            cur_params.update({key_params: value_params})
+            data_for_name.update({name:cur_params})
+        if mr_wrong:
+            break
+    position_free = all_position- position_find
+    return data_position, data_link_name, mr_wrong, list_name_same_position, all_name, position_free, all_position
 
 def get_params_str(str_in, queue_length):
     int_str = re.findall(r'\d+', str_in)
@@ -85,58 +121,46 @@ def get_params_str(str_in, queue_length):
     else:
         if "The man behind me is" in str_in:
             value_params = str_in.split("The man behind me is ")[1][:-1]
-            key_params = "position_plus_name"
+            key_params = "position_plus"
         elif "The man in front of me is " in str_in:
             value_params = str_in.split("The man in front of me is ")[1][:-1]
-            key_params = "position_minus_name"
-    return {key_params: value_params}
+            key_params = "position_minus"
+    return key_params,value_params
+
+def get_exist_position(data_in):
+    exist_position = []
+    for name, position_data in data_in.items():
+        exist_position.append(position_data["position"])
+    return set([i["position"] for i in data_in.values()])
 
 
-def use_rule_and_assert_composition(data_in, rule):
-    print()
+get_exist_position({'John': {'position': 1}, 'Peter': {'position': 2}, 'Tom': {'position': 1}})
+print()
+def assert_position_rule(data_position, data_link, position_free, all_position, name_assert):
+    for name, data in data_link.items():
+        list_name_neighbour = [data.get("position_plus"), data.get("position_minus")]
+        for idx, name_neighbour in enumerate(list_name_neighbour):
+            if name_neighbour == name_assert:
+                plus = -1 if idx == 0 else 1
+                print()
 
 
-def get_data_position_rule_and_assert(data_in):
-    list_names = list(data_in.keys())
-    len_names = len(list_names)
-    wrong_position = 0
-    name_with_position = {}
-    name_not_position = {}
-    potential_mr_wrong = []
-    potential_mr_right = []
-    for idx, data_name in enumerate(data_in.items()):
-        name, data = data_name
-        position = data.get("position")
-        potential_mr_right.append(name)
-        if position:
-            wrong_position = position if not (0 <= position <= len_names) else wrong_position
-            exist_name = name_with_position.get(position)
-            if exist_name:
-                name_add = exist_name + [name]
-                name_with_position[position] = name_add
-                potential_mr_wrong.extend(name_add)
-                del potential_mr_right[idx]
-            else:
-                name_with_position[position] = [name]
-        else:
-            name_not_position.update({name: data})
-    position_found = list(name_with_position.keys())
-    assert_out = [wrong_position, # Условие на невозможную позицию
-                  len(position_found) == len_names,  # условие если всех распределили по позициям
-                  len(potential_mr_wrong) > 2,  # Условие если лжецов больше 2
-                  len(potential_mr_right) + len(potential_mr_wrong) >
-                  len_names and not name_not_position # условие если нет людей без позиции,
-                   # но при этом количесрво потенциных лжецов и правдивых больше списка
-                       ]
-    return assert_out,  name_with_position, name_not_position, potential_mr_wrong, potential_mr_right
 
 def find_out_mr_wrong(conversation):
-    data_for_name = get_data_for_list_str(conversation)
-    assert_composition, name_with_position, name_not_position, potential_mr_wrong, potential_mr_right = (
-        get_data_position_rule_and_assert(data_for_name))
-    if any(assert_composition):
-        return None
-    print()
+    data_position, data_link_name, mr_wrong, list_name_same_position, all_name, position_free, all_position = (
+        get_data_for_list_str(conversation))
+    if mr_wrong:
+        return mr_wrong
+    if list_name_same_position:
+        for name_same_position in list_name_same_position:
+            res = assert_position_rule(data_position, data_link_name, position_free, all_position,
+                                       name_same_position)
+
+
+    # print(assert_composition)
+    # print(f"Имена без позиций: {name_not_position}")
+    # print(f"Имена с позициями: {name_with_position}")
+    # print()
 
 for idx, conversation in enumerate(list_conversation):
     print(f"НАБОР НОМЕР {idx+1}")
